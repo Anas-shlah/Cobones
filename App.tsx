644da04c5fd1db93728d10e981f8f34a970c5b91
Anas shlah <anis.sh93@gmail.com>
2024-07-05 21:01:18 +0300

    add react native navigation and test it

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar} from 'react-native';
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import MainNavigator from './src/MainNavigator';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <View>
-        <Text>App</Text>
-      </View>
+      <NavigationContainer>
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+        <MainNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
