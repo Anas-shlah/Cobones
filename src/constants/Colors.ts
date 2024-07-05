b1331edc25c9cff7d82a79b0ff79e45a7f662fff
Anas shlah <anis.sh93@gmail.com>
2024-07-05 18:54:41 +0300

    add constants

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  background: '#FFFFFF',
+  primary: '#dbb764',
+  secondary: 'rgb(46, 53, 78)',
+  black: '#000',
+  text: '#000',
+  textSecondary: '#696d7c',
+  textPlaceholder: '#ddd',
+  iconDisabled: '#535353',
+  white: '#FFFFFF',
+};
--- src/constants/Layout.ts
+++ src/constants/Layout.ts
@@ -1,1 +1,21 @@
-export default {};
+import {Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+export default {
+  window: {
+    width,
+    height,
+  },
+  button: 56,
+  spacing: {
+    xxs: 1,
+    xs: 2,
+    sm: 8,
+    md: 12,
+    lg: 16,
+    xl: 20,
+    xxl: 24,
+    xxxl: 30,
+  },
+};
