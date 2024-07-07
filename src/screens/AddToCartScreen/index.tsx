b3c92be1298e37c29cded281641de1a4061c1b5f
Anas shlah <anis.sh93@gmail.com>
2024-07-07 09:02:10 +0300

    create Pay Button

--- src/screens/AddToCartScreen/index.tsx
+++ src/screens/AddToCartScreen/index.tsx
@@ -3,17 +3,40 @@ import React from 'react';
 import CardProduct from '../../components/CardProduct';
 import DiscountAndGift from '../../components/DiscountAndGift';
 import PaymentDetails from './PaymentDetails';
+import Colors from '../../constants/Colors';
+import {Text} from '../../components/Text';
+import {Button} from '../../components/Button';
+import Layout from '../../constants/Layout';
+import {translation} from '../../locales';
 
 const AddToCartScreen = ({route}) => {
   const data = route.params;
   return (
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={{paddingBottom: 50}}>
-        <CardProduct data={data} />
-        <DiscountAndGift style={styles.input} />
-        <DiscountAndGift preset="gift" />
-        <PaymentDetails value={data.price} />
-      </ScrollView>
+    <View>
+      <View style={styles.container}>
+        <ScrollView
+          contentContainerStyle={{paddingBottom: 50}}
+          style={{height: Layout.window.height - Layout.window.height / 5}}
+        >
+          <CardProduct data={data} />
+          <DiscountAndGift style={styles.input} />
+          <DiscountAndGift preset="gift" />
+          <PaymentDetails value={data.price} />
+        </ScrollView>
+      </View>
+      <View style={styles.payButton}>
+        <View>
+          <Text preset="secondary">
+            {translation.screens.Cart['total-price']}
+          </Text>
+          <Text preset="header">AED {data.price}</Text>
+        </View>
+        <Button
+          preset="secondary"
+          title={translation.screens.Cart['proceed-to-pay']}
+          style={{width: '60%'}}
+        />
+      </View>
     </View>
   );
 };
@@ -26,5 +49,12 @@ const styles = StyleSheet.create({
   },
   input: {
     marginVertical: 30,
+  },
+  payButton: {
+    backgroundColor: Colors.white,
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 });
