0aed17973fbf627aa37df8b6f2cdffa334394a84
Anas shlah <anis.sh93@gmail.com>
2024-07-07 08:29:57 +0300

    create inputs coupon

--- src/screens/AddToCartScreen/index.tsx
+++ src/screens/AddToCartScreen/index.tsx
@@ -1,13 +1,16 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import React from 'react';
 import CardProduct from '../../components/CardProduct';
+import DiscountAndGift from '../../components/DiscountAndGift';
 
-const AddToCartScreen = ({route, navigation}) => {
+const AddToCartScreen = ({route}) => {
   const data = route.params;
   return (
     <View style={styles.container}>
       <ScrollView>
         <CardProduct data={data} />
+        <DiscountAndGift style={styles.input} />
+        <DiscountAndGift preset="gift" />
       </ScrollView>
     </View>
   );
@@ -18,5 +21,8 @@ export default AddToCartScreen;
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+  },
+  input: {
+    marginVertical: 30,
   },
 });
